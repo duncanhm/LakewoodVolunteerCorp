eaef9385b367318841905929e0eee1f6177709e6
Duncan Howard-McGuire <d.t.howardmcguire@gmail.com>
2018-06-04 12:52:27 -0700

    Added DisplayName attributes to Volunteer class properties

--- LakewoodVolunteerCorp/Models/Volunteer.cs
+++ LakewoodVolunteerCorp/Models/Volunteer.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace LakewoodVolunteerCorp.Models
 {
     public class Volunteer
     {
         public int ID { get; set; }
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+        [DisplayName("Sign Up Date")]
         public DateTime SignUpDate { get; set; }
 
         public virtual ICollection<SignUp> SignUps { get; set; }
